f3a68ffb0a83f15e1327ecec4077dccf29d42f25
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2025-01-06 17:41:12 +0000

    Adapt to SeroBA v2.0+ output format

--- bin/get_serotype.sh
+++ bin/get_serotype.sh
@@ -1,6 +1,6 @@
 # Run SeroBA to serotype samples
 
-seroba runSerotyping "${SEROBA_DB}" "$READ1" "$READ2" "$SAMPLE_ID" && SEROTYPE=$(awk -F'\t' '{ print $2 }' "${SAMPLE_ID}/pred.tsv")
+seroba runSerotyping "${SEROBA_DB}" "$READ1" "$READ2" "$SAMPLE_ID" && SEROTYPE=$(awk -F',' 'NR==2 { print $3 }' "${SAMPLE_ID}/pred.csv")
 
 echo \"Serotype\" > "$SEROTYPE_REPORT"
 echo \""$SEROTYPE"\" >> "$SEROTYPE_REPORT"
