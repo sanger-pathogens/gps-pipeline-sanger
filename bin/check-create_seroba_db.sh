643aa4d95588fc146f067fdd4fae422f55379b6d
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2025-08-04 15:11:11 +0000

    Change to md5 checksum validation

--- bin/check-create_seroba_db.sh
+++ bin/check-create_seroba_db.sh
@@ -1,4 +1,5 @@
 # Check if database was downloaded from specific link, also prepared by the specific Kmer
+# Check if all files exist and data integrity is not compromised
 # If not: remove files in database directory and download, re-create KMC and ARIBA databases, also save metadata to JSON
 
 ZIPPED_REPO='seroba.tar.gz'
@@ -6,14 +7,8 @@ ZIPPED_REPO='seroba.tar.gz'
 if  [ ! -f "${DB_LOCAL}/${JSON_FILE}" ] || \
     [ ! "$(grep '"url"' "${DB_LOCAL}/${JSON_FILE}" | sed -r 's/.+: "(.*)",?/\1/')" == "$DB_REMOTE" ] || \
     [ ! "$(grep '"kmer"' "${DB_LOCAL}/${JSON_FILE}" | sed -r 's/.+: "(.*)",?/\1/')" == "$KMER" ] || \
-    [ ! -d "${DB_LOCAL}/ariba_db" ] || \
-    [ ! -d "${DB_LOCAL}/kmer_db" ] || \
-    [ ! -d "${DB_LOCAL}/streptococcus-pneumoniae-ctvdb"] || \
-    [ ! -f "${DB_LOCAL}/cd_cluster.tsv" ] || \
-    [ ! -f "${DB_LOCAL}/cdhit_cluster" ] || \
-    [ ! -f "${DB_LOCAL}/kmer_size.txt" ] || \
-    [ ! -f "${DB_LOCAL}/meta.tsv" ] || \
-    [ ! -f "${DB_LOCAL}/reference.fasta" ]; then
+    [ ! -f "${DB_LOCAL}/${CHECKSUM_FILE}" ] || \
+    ! ( cd "${DB_LOCAL}" && md5sum -c "${CHECKSUM_FILE}" ) ; then
 
     rm -rf "${DB_LOCAL}"
 
@@ -28,6 +23,8 @@ if  [ ! -f "${DB_LOCAL}/${JSON_FILE}" ] || \
     seroba createDBs "${DB_LOCAL}" "${KMER}"
 
     rm -f $ZIPPED_REPO
+
+    ( cd "${DB_LOCAL}" && find . -type f -not -name "${CHECKSUM_FILE}" -exec md5sum "{}" + ) > "${DB_LOCAL}/${CHECKSUM_FILE}"
 
     echo -e "{\n  \"url\": \"$DB_REMOTE\",\n  \"kmer\": \"$KMER\",\n  \"create_time\": \"$(date +"%Y-%m-%d %H:%M:%S %Z")\"\n}" > "${DB_LOCAL}/${JSON_FILE}"
 
